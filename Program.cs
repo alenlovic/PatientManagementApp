5b4b32e2fdc81e251af85725c2062e637ac7bed8
alenlovic <alenlovic1987@gmail.com>
2024-08-17 14:11:31 +0200

    added sql connection string in program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using PatientManagementApp.Database;
+
 namespace PatientManagementApp
 {
     public class Program
@@ -13,6 +16,8 @@ namespace PatientManagementApp
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("sqlconnect")));
 
             var app = builder.Build();
 
