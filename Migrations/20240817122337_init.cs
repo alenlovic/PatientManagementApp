d882fb69a7836c1857cc64c6641b6e5f441e092f
alenlovic <alenlovic1987@gmail.com>
2024-08-17 14:30:05 +0200

    merge entities in sqlserver database, db configuration

---
+++ Migrations/20240817122337_init.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatientManagementApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    PatientId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FathersName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YearOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PlaceOfBirth = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostalAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JMBG = table.Column<long>(type: "bigint", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    isCritical = table.Column<bool>(type: "bit", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.PatientId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Billing",
+                columns: table => new
+                {
+                    BillingId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PatientId = table.Column<int>(type: "int", nullable: false),
+                    PaymentMethod = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CurrentAmount = table.Column<int>(type: "int", nullable: false),
+                    DateOfLastPayment = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RemainingAmount = table.Column<int>(type: "int", nullable: false),
+                    BillingStatus = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Billing", x => x.BillingId);
+                    table.ForeignKey(
+                        name: "FK_Billing_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "PatientId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PatientAppointmentEntity",
+                columns: table => new
+                {
+                    PatientAppointmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PatientId = table.Column<int>(type: "int", nullable: false),
+                    Appointment = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AppointmentNote = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatientAppointmentEntity", x => x.PatientAppointmentId);
+                    table.ForeignKey(
+                        name: "FK_PatientAppointmentEntity_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "PatientId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PatientRecords",
+                columns: table => new
+                {
+                    PatientRecordId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PatientId = table.Column<int>(type: "int", nullable: false),
+                    DentalProsthetics = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PreviousDiseases = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ChronicDiseases = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Allergies = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PenicilinAllergy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OPG = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatientRecords", x => x.PatientRecordId);
+                    table.ForeignKey(
+                        name: "FK_PatientRecords_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "PatientId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Billing_PatientId",
+                table: "Billing",
+                column: "PatientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PatientAppointmentEntity_PatientId",
+                table: "PatientAppointmentEntity",
+                column: "PatientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PatientRecords_PatientId",
+                table: "PatientRecords",
+                column: "PatientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Billing");
+
+            migrationBuilder.DropTable(
+                name: "PatientAppointmentEntity");
+
+            migrationBuilder.DropTable(
+                name: "PatientRecords");
+
+            migrationBuilder.DropTable(
+                name: "Patients");
+        }
+    }
+}
--- Models/PatientEntity.cs
+++ Models/PatientEntity.cs
@@ -16,5 +16,7 @@ namespace PatientManagementApp.Models
         public string PhoneNumber { get; set; }
         public long JMBG { get; set; } 
         public string Email { get; set; }
+        public bool isCritical { get; set; }
+        public string Note { get; set; }
     }
 }
